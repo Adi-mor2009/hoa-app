76584c8d422436f8cb8d3531c3ec5cc12f16380b
Adi-mor2009 <adijmy@gmail.com>
2021-05-07 10:33:55 +0300

    adding basic navbar to home page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
+import MainNavbar from './components/MainNavbar/MainNavbar';
 
 function App() {
   return (
     <div className="app">
       <HashRouter>
         <Switch>
-          <Route exact path="/" ><HomePage/></Route>
+          <Route exact path="/" >
+            <MainNavbar/>
+            <HomePage/>
+          </Route>
           <Route exact path="/login"><LoginPage/></Route>
           <Route exact path="/signup" ><SignupPage/></Route>
-          {/* <Route exact path="/dashboard" ><DashboardPage/></Route>
+          {/* <Route exact path="/dashboard" >
+            <RecipeNavbar/>
+            <DashboardPage/>
+          </Route>
           <Route exact path="/tenants" ><TenantsPage/></Route>
           <Route exact path="/messages" ><MessagesdPage/></Route>
           <Route exact path="/new-message" ><NewMessagePage/></Route>
---
+++ src/components/MainNavbar/MainNavbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+function MainNavbar(props) {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#/">HOA</Navbar.Brand>
+            {/* <Navbar.Brand href="#/">Recipe Book</Navbar.Brand> */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#/dashboard">Dashboard</Nav.Link>
+                </Nav>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#/login">Login</Nav.Link>
+                    <Nav.Link href="#/signup">Signup</Nav.Link>
+                    <Nav.Link href="#">Logout</Nav.Link>
+                </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default MainNavbar;
